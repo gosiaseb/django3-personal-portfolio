f1ef41c2b6ee4e6529dba6ae7c47cc3ccc64ee92
gosiaseb <msebastiampillai@datapebbles.com>
2022-01-19 08:31:22 +0000

    untrack files in gitignore

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-19-0atje6tfv5#js57u75)@myatgy^_qmqsx76=ly&e&*-qd@s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['msebastiampillai.pythonanywhere.com']
 
 
 # Application definition
@@ -118,9 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
